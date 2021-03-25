c822f5104d423f79c5017dd315e08f2d0dfb1887
Razvan Filea <razvan.filea@gmail.com>
2021-03-25 14:36:07 +0200

    Fix Build Errors

--- app/src/main/java/net/theluckycoder/stundenplan/utils/AppPreferences.kt
+++ app/src/main/java/net/theluckycoder/stundenplan/utils/AppPreferences.kt
@@ -3,26 +3,26 @@ package net.theluckycoder.stundenplan.utils
 import android.content.Context
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.createDataStore
+import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import net.theluckycoder.stundenplan.TimetableType
 
-class AppPreferences(context: Context) {
+private val Context.appDataStore by preferencesDataStore(AppPreferences.DATA_STORE_NAME)
 
-    private val userTeamDataStore = context.createDataStore(name = DATA_STORE_NAME)
+class AppPreferences(private val context: Context) {
 
-    val darkThemeFlow: Flow<Boolean> = userTeamDataStore.data
+    val darkThemeFlow: Flow<Boolean> = context.appDataStore.data
         .map { preferences ->
             preferences[DARK_THEME] ?: false
         }.distinctUntilChanged()
 
-    suspend fun updateUseDarkTheme(useDarkTheme: Boolean) = userTeamDataStore.edit { preferences ->
+    suspend fun updateUseDarkTheme(useDarkTheme: Boolean) = context.appDataStore.edit { preferences ->
         preferences[DARK_THEME] = useDarkTheme
     }
 
-    val timetableTypeFlow: Flow<TimetableType> = userTeamDataStore.data
+    val timetableTypeFlow: Flow<TimetableType> = context.appDataStore.data
         .map { preferences ->
             if (preferences[TIMETABLE_TYPE] == false)
                 TimetableType.MIDDLE_SCHOOL
@@ -30,12 +30,12 @@ class AppPreferences(context: Context) {
                 TimetableType.HIGH_SCHOOL
         }.distinctUntilChanged()
 
-    suspend fun updateTimetableType(timetableType: TimetableType) = userTeamDataStore.edit { preferences ->
+    suspend fun updateTimetableType(timetableType: TimetableType) = context.appDataStore.edit { preferences ->
         preferences[TIMETABLE_TYPE] = timetableType == TimetableType.HIGH_SCHOOL
     }
 
     companion object {
-        private const val DATA_STORE_NAME = "user_prefs"
+        const val DATA_STORE_NAME = "user_prefs"
 
         private val DARK_THEME = booleanPreferencesKey("dark_theme")
         private val TIMETABLE_TYPE = booleanPreferencesKey("timetable_type")
