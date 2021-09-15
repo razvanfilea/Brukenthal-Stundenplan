6780a8490ab5ee04813e18bed049d319a8bcbb09
Deborah Florea <deborahflorea97@gmail.com>
2021-09-15 20:25:38 +0300

    Almost finished
    Everything's working except draggable

--- app/src/main/java/net/theluckycoder/stundenplan/extensions/Extensions.kt
+++ app/src/main/java/net/theluckycoder/stundenplan/extensions/Extensions.kt
@@ -1,7 +1,13 @@
 package net.theluckycoder.stundenplan.extensions
 
 import android.app.Application
+import androidx.compose.ui.geometry.Offset
 import androidx.lifecycle.AndroidViewModel
 
 val AndroidViewModel.app: Application
     get() = getApplication()
+
+fun Offset.coerceIn(min: Offset, max: Offset) = Offset(
+    x = x.coerceIn(min.x, max.x),
+    y = y.coerceIn(min.y, max.y),
+)
